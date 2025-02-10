024c79e03dbed87b261b206cef8c224aa249e68b
uncle12genius <omondievan461@gmail.com>
2025-02-10 18:15:04 +0300

    user input for java

--- src/Practice.java
+++ src/Practice.java
@@ -1,5 +1,12 @@
-public class Practice {
-
-
-
+import java.util.Scanner;
+public class Practice  {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = reader.nextInt();
+        if(num % 2 == 0)
+            System.out.println(num + " is even");
+        else
+            System.out.println(num + " is odd");
+    }
 }
