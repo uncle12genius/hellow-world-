cdd9f23cae980dc5c6cdc51773e145fd6347489b
uncle12genius <omondievan461@gmail.com>
2025-03-23 20:19:57 +0300

    user input for java

--- src/user.java
+++ src/user.java
@@ -1,12 +1,20 @@
-import java .util.Scanner;
-public class user{
- public static void main  (String[]args){
-     Scanner name = new Scanner(System.in);
-     System.out.println("enter your name ");
+import java.util.Scanner;
+public class user {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-     String userName = name.nextLine();
-     System.out.println("username is :"+ userName);
+        System.out.println("enter your age ?");
+        int Age = sc.nextInt();
 
-     name.close ();
- }
+        if(Age<= 18 ){
+            System.out.println("Your are  not allowed to vote ");
+
+
+        }else {
+            System.out.println("Your are allowed to vote ");
+        }
+
+        sc.close();
+    }
+
 }
